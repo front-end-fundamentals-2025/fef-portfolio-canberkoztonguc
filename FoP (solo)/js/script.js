d026aa35f8e8eabc9cf8547dcb4d7959d8566ded
canberkoztonguc <ozca24tl@student.ju.se>
2025-06-01 13:28:52 +0200

    Delete FoP (solo) directory

--- FoP (solo)/js/script.js
+++
@@ -1,9 +0,0 @@
-
-const projectCards = document.querySelectorAll('.project-card');
-
-projectCards.forEach(card => {
-  card.addEventListener('click', function (event) {
-    const projectName = this.querySelector('p').textContent;
-    alert(`You clicked on: ${projectName}`);
-  });
-});
